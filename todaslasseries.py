1a0b650caee428a5cd3f79a68b06f1ef0239536d
Maximiliano Semcoff <92069929+msms1991@users.noreply.github.com>
2022-01-26 15:00:23 -0300

    Add files via upload
    Agrego los archivos .csv

---
+++ todaslasseries.py
@@ -0,0 +1,32 @@
+from operator import index
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+import pandas
+
+
+PATH = '/Users/maxi_/Desktop/Driver/chromedriver'
+
+driver = webdriver.Chrome(PATH)
+
+driver.get('https://www.starz.com/ar/es/view-all/blocks/1523514')
+
+time.sleep(5)
+
+
+for X in range(1,25):
+    for Y in range(1,5):
+        xpath = f'/html/body/starz-root/div/div[1]/section/starz-view-all/div/div/div/div/section[1]/virtual-scroller/div[2]/div[{X}]/div[{Y}]/div/starz-content-item/article/div[1]/a[1]/h3'
+        series = driver.find_element_by_xpath(xpath).get_attribute('textContent')
+        print("Nombre de serie: ",series)
+
+serieslista= []
+for X in range(1,25):
+    for Y in range(1,5):
+        xpath = f'/html/body/starz-root/div/div[1]/section/starz-view-all/div/div/div/div/section[1]/virtual-scroller/div[2]/div[{X}]/div[{Y}]/div/starz-content-item/article/div[1]/a[1]/h3'
+        series = driver.find_element_by_xpath(xpath).get_attribute('textContent')
+        serieslista.append(driver.find_element_by_xpath(xpath).get_attribute('textContent'))
+
+df = pandas.DataFrame({'Series':pandas.Series})
+print(df)
+df.to_csv('Series.csv', index=False)
